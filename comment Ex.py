512dfbc8a86c8a0f1e39f223a17cecabb303e18b
Hong-825 <1emoncat1412@gmail.com>
2020-12-01 22:02:49 +0900

    join 1

--- comment Ex.py
+++ comment Ex.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from tkinter import *
 from tkinter import ttk
 from passlist import *
-from commentscript import *
+# from commentscript import *
 import time
 import pyperclip
 
@@ -93,17 +93,108 @@ def login():
             if nickname not in passlist:
                 # 좋아요 없는 게시판
                 if board == 74:
-                    addJoinus(i)
+                    driver.switch_to_default_content()
+                    # 본인 닉네임
+                    getYournickname = driver.execute_script(
+                        'return document.querySelector("#gnb_name1").innerText')
+                    driver.switch_to_frame("cafe_main")
+                    # 작성자 등급확인
+                    getRating = driver.execute_script(
+                        f'return document.querySelector("#main-area > div:nth-child(6) > table > tbody > tr:nth-child({i}) > td.td_name > div > table > tbody > tr > td > span > img").src')
+                    # 등급 1 확인
+                    if getRating == "https://cafe.pstatic.net/levelicon/1/1_110.gif":
+                        driver.execute_script(
+                            f'document.querySelector("#main-area > div:nth-child(6) > table > tbody > tr:nth-child({i}) > td.td_article > div.board-list > div > a").click()')
+                        time.sleep(2)
+                        # 댓글 작성자 리스트
+                        setAuthorList = driver.find_elements_by_css_selector(
+                            ".comment_nickname")
+                        alterAuthorList = []
+                        for x in setAuthorList:
+                            alterAuthorList.append(x.text())
+                        if alterAuthorList.indexOf() == -1:
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > a").click()')
+                            time.sleep(1)
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > div > div > div > div > ul > li.active > div > ul > li:nth-child(12) > button").click()')
+                            time.sleep(1)
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.register_box > a").click()')
+                            time.sleep(2)
+                            driver.refresh()
+                            time.sleep(3)
+                        else:
+                            driver.refresh()
+                            time.sleep(1)
+                            continue
+                    else:
+                        driver.refresh()
+                        time.sleep(1)
+                        continue
                 else:
                     # 매매일지 or 모바일 수익
                     if board == 189 or board == 940:
-                        addTradinglogComments(i)
+                        # 페이지 상단글부터 클릭
+                        driver.execute_script(
+                            f'document.querySelector("#main-area > div:nth-child(6) > table > tbody > tr:nth-child({i}) > td.td_article > div.board-list > div > a").click()')
+                        time.sleep(2)
+                        # 좋아요 값 확인
+                        like = driver.execute_script(
+                            "return document.querySelector('#app > div > div > div.ArticleContentBox > div.article_container > div.ReplyBox > div.box_left > div > div > a').getAttribute('aria-pressed')")
+                        # 좋아요가 눌러져있으면 종료.
+                        if like == 'true':
+                            driver.quit()
+                        else:
+                            # 좋아요 클릭
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.ReplyBox > div.box_left > div > div > a > span").click()')
+                            time.sleep(1)
+                            # 스티커 박스 클릭
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > a").click()')
+                            time.sleep(1)
+                            # 스티커 선택
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > div > div > div > div > ul > li.active > div > ul > li:nth-child(6) > button").click()')
+                            time.sleep(1)
+                            # 등록 클릭
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.register_box > a").click()')
+                            time.sleep(2)
+                            # 새로고침해서 밖으로 빠져나가기
+                            driver.refresh()
+                            time.sleep(3)
+                            continue
                     # 뉴스
                     elif board == 195:
-                        addNewsComments(i)
+                        driver.execute_script(
+                            f'document.querySelector("#main-area > div:nth-child(6) > table > tbody > tr:nth-child({i}) > td.td_article > div.board-list > div > a").click()')
+                        time.sleep(2)
+                        like = driver.execute_script(
+                            "return document.querySelector('#app > div > div > div.ArticleContentBox > div.article_container > div.ReplyBox > div.box_left > div > div > a').getAttribute('aria-pressed')")
+                        if like == 'true':
+                            driver.quit()
+                        else:
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.ReplyBox > div.box_left > div > div > a > span").click()')
+                            time.sleep(1)
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > a").click()')
+                            time.sleep(1)
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.attach_box > div > div > div > div > ul > li.active > div > ul > li:nth-child(5) > button").click()')
+                            time.sleep(1)
+                            driver.execute_script(
+                                'document.querySelector("#app > div > div > div.ArticleContentBox > div.article_container > div.CommentBox > div.CommentWriter > div.comment_attach > div.register_box > a").click()')
+                            time.sleep(2)
+                            driver.refresh()
+                            time.sleep(3)
+                            continue
             else:
                 driver.refresh()
                 time.sleep(1)
+                continue
 
 
 # login 버튼
